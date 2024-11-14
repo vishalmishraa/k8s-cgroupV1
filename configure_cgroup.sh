90a81a678c29c12399d428f17ec0dd104374b29e
vishalmishraa <vishal.mishra9470@gmail.com>
2024-11-14 18:37:25 +0530

    init

---
+++ configure_cgroup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if the cgroup configuration is already set
+if grep -q "systemd.unified_cgroup_hierarchy=0" /etc/default/grub; then
+    echo "Cgroup configuration already set."
+    exit 0
+fi
+
+# Add the cgroup configuration
+sudo sed -i 's/GRUB_CMDLINE_LINUX="/&systemd.unified_cgroup_hierarchy=0 /' /etc/default/grub
+
+# Update GRUB
+sudo update-grub
+
+# Reboot the node
+sudo reboot
