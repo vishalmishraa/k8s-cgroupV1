f939c2fefe39c645b75fe796bab339386e5f4a3b
vishalmishraa <vishal.mishra9470@gmail.com>
2024-11-14 19:59:57 +0530

    Improve cgroup configuration script: enhance reboot messages and ensure proper reboot command usage

--- configure_cgroup.sh
+++ configure_cgroup.sh
@@ -2,9 +2,11 @@
 
 # Check if the cgroup configuration is already set
 if grep -q "systemd.unified_cgroup_hierarchy=0" /etc/default/grub; then
-    echo "The cgroup configuration is already set updating greb and rebooting the node"
+    echo "The cgroup configuration is already set, updating grub and rebooting the node"
     update-grub
-    reboot
+    echo "grub updated, rebooting the node"
+    /sbin/reboot
+    exit
 fi
 
 # Add the cgroup configuration
@@ -14,4 +16,4 @@ sed -i 's/GRUB_CMDLINE_LINUX="/&systemd.unified_cgroup_hierarchy=0 /' /etc/defau
 update-grub
  
 # Reboot the node
-reboot
+/sbin/reboot
