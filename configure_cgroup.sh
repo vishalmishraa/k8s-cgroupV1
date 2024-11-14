b5ea3ee66275147805306aefba7b3fad55685fdb
vishalmishraa <vishal.mishra9470@gmail.com>
2024-11-14 20:06:11 +0530

    update

--- configure_cgroup.sh
+++ configure_cgroup.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
-
 # Check if the cgroup configuration is already set
 if grep -q "systemd.unified_cgroup_hierarchy=0" /etc/default/grub; then
-    echo "The cgroup configuration is already set, updating grub and rebooting the node"
-    update-grub
-    echo "grub updated, rebooting the node"
-    /sbin/reboot
+    echo "The cgroup configuration is already set, updating grub"
+    if update-grub; then
+        echo "grub updated"
+    else
+        echo "Failed to update grub"
+    fi
+    echo "Rebooting the node"
+    if command -v reboot &> /dev/null; then
+        reboot
+    else
+        echo "Reboot command not found"
+    fi
     exit
 fi
-
 # Add the cgroup configuration
 sed -i 's/GRUB_CMDLINE_LINUX="/&systemd.unified_cgroup_hierarchy=0 /' /etc/default/grub
-
 # Update GRUB
-update-grub
- 
+if update-grub; then
+    echo "grub updated"
+else
+    echo "Failed to update grub"
+fi
+
 # Reboot the node
-/sbin/reboot
+echo "Rebooting the node"
+if command -v reboot &> /dev/null; then
+    reboot
+else
+    echo "Reboot command not found"
+fi
